d7dddbe630c1a2047b18afaa7afade9eb13205dc
Kate <kowens711@gmail.com>
2017-04-25 11:42:09 -0400

    Initial Commit

---
+++ TimeAfterTime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Kate Owens  on 4/25/17.
+//  Copyright © 2017 Me. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
