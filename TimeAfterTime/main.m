1d137f6f0c304cd83902148c34da537c63a237cd
Kate <kowens711@gmail.com>
2017-04-26 08:40:18 -0400

    KO - added NSDate and set to 100,000 seconds from other NSDate instance.

--- TimeAfterTime/main.m
+++ TimeAfterTime/main.m
@@ -17,6 +17,9 @@ int main(int argc, const char * argv[]) {
         double seconds = [now timeIntervalSince1970];
         NSLog(@"It has been %f seconds since the start of 1970.", seconds);
         
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds it will be %@", later);
+        
         NSHost *computerInformation = [NSHost currentHost];
         [computerInformation localizedName];
         NSString *computerName = [computerInformation localizedName];
